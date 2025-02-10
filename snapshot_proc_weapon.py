da2db915d91c195a5a47ebd9f435c8fd40c4c969
bearkim <kty@univs.ai>
2025-02-10 10:52:05 +0900

    aa

--- snapshot_proc_weapon.py
+++ snapshot_proc_weapon.py
@@ -98,7 +98,8 @@ def is_overlap(boxA, boxB):
 
 async def main():
 
-    model = YOLO('yolo11l.pt', verbose=False)  # COCO 사전 학습
+    aa = torch.load("best.pt", weights_only=False)
+    model = YOLO(aa)  # COCO 사전 학습
     model.overrides['conf'] = 0.25  # confidence threshold 설정
     model.overrides['imgsz']=1024
     
